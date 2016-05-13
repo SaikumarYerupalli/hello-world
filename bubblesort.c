a9e5e8102acf0a2bd4857a4bdb9bf8f8acb89541
SaikumarYerupalli <sai.yerupalli@gmail.com>
2016-05-13 06:05:17 -0700

    Add files via upload

---
+++ bubblesort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void bubblesort(int *arr,int size)
+{
+	int i,j,temp,flag=0;
+	printf("%d %d %d %d %d",*arr,*(arr++),*(arr++),*(arr++),*(arr++));
+	for(i=0;i<size;i++)
+	{
+		for(j=i+1;j<size;j++)
+		{
+			if(arr[j]<arr[i])
+			{
+				flag=1;
+				temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
+}
+int main()
+{
+	int a[10],i,size;
+	printf("Enter size of array \n");
+	scanf("%d",&size);
+	printf("Enter array\n");
+	for(i=0;i<size;i++)
+	scanf("%d",&a[i]);
+	bubblesort(a,size);
+	printf("Enter sorting");
+	for(i=0;i<size;i++)
+	printf("%d\n",a[i]);
+	return 0;
+}
